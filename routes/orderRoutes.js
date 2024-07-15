95949def04da16052ff6b1e6bb973da64c8bea1d
mohammad noor <g.s.dadanoor@gmail.com>
2024-07-15 14:46:32 +0530

    5

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const orderController = require('../Controllers/orderController');
 
-router.post('/Place Order', orderController.placeOrder);
+router.post('/PlaceOrder', orderController.placeOrder);
 
 module.exports = router;
